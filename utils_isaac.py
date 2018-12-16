82dfc62c57b73d98f72bf388ca9c84af2639f2f4
ialb97 <ialb998@gmail.com>
2018-12-16 13:07:46 -0500

    enroll function

--- utils_isaac.py
+++ utils_isaac.py
@@ -1,26 +1,34 @@
-def enrol(sid,cid):
-	raise NotImplementedError
+	def enrol(self,sid,cid):
+		seatstaken = self.session.query(func.count(self.taking.sid)).filter(cid=cid)
+		totalseats = self.session.query(self.classes.max_students).filter(cid=cid)
+		if totalseats - seatstaken > 0:
+			new_taking = self.taking(taid = None,sid=sid,cid=cid)
+			self.session.add(new_taking)
+			self.session.commit()
+			return
+		else:
+			raise ValueError("No more Seats Available: Not Enrolled")
 
-def drop(sid,cid):
-	raise NotImplementedError
+	def drop(self,sid,cid):
+		raise NotImplementedError
 
-def change_salary(uid, new_salary):
-	raise NotImplementedError
+	def change_salary(self,uid, new_salary):
+		raise NotImplementedError
 
-def get_schedule(sid,semester):
-	raise NotImplementedError
+	def get_schedule(self,sid,semester):
+		raise NotImplementedError
 
-def get_grades(sid, semeter=None):
-	raise NotImplementedError
+	def get_grades(self,sid, semeter=None):
+		raise NotImplementedError
 
-def get_class_info(cid):	
-	raise NotImplementedError
+	def get_class_info(self,cid):	
+		raise NotImplementedError
 
-def get_prof_info(pid):
-	raise NotImplementedError
+	def get_prof_info(self,pid):
+		raise NotImplementedError
 
-def get_student_info(sid):
-	raise NotImplementedError
+	def get_student_info(self,sid):
+		raise NotImplementedError
 
-def get_admin_info(uid):
-	raise NotImplementedError
+	def get_admin_info(self,uid):
+		raise NotImplementedError
