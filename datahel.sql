f6676d1b6319d2e1db1777a6a06f0bf81c60da7b
gylee1618 <horseantson@hotmail.com>
2018-12-16 14:50:37 -0500

    set up merge

--- datahel.sql
+++ datahel.sql
@@ -47,7 +47,7 @@ CREATE TABLE teaching ( teid INT  NOT NULL AUTO_INCREMENT,
 	pid INT,
 	cid INT,
 	PRIMARY KEY (teid),
-	FOREIGN KEY (pid) REFERENCES professors(pid),
+	FOREIGN KEY (pid) REFERENCES professors(uid),
 	FOREIGN KEY (cid) REFERENCES classes(cid)
 );
 
@@ -57,7 +57,7 @@ CREATE TABLE taking ( taid INT NOT NULL AUTO_INCREMENT,
 	grade FLOAT,
 	PRIMARY KEY (taid),
 	FOREIGN KEY (cid) REFERENCES classes(cid),
-	FOREIGN KEY (sid) REFERENCES students(sid),
+	FOREIGN KEY (sid) REFERENCES students(uid),
 	CHECK (grade <= 4),
 	CHECK (grade >= 0)
 );
@@ -79,9 +79,9 @@ insert into classes values (464, 'ECE464', 'Fall 2018', 'Tue 1800-2050','Electri
 insert into classes values (2231, 'PH351', 'Fall 2018', 'Tue 0900-1150','Physics', 3.0, 20);
 insert into classes values (227, 'ECE464', 'Fall 2016', 'Wed 1600-1750, Thu 1200-1250','Electrical Engineering', 3.0, 30);
 
-insert into teaching values (1, 1, 464);
-insert into teaching values (2, 2, 2231);
-insert into teaching values (3, 1, 227);
+insert into teaching values (1, 3, 464);
+insert into teaching values (2, 4, 2231);
+insert into teaching values (3, 3, 227);
 
 insert into taking values (1, 2, 227, 3);
 insert into taking values (2, 2, 2231, NULL);
--- utils.py
+++ utils.py
@@ -14,6 +14,7 @@ class DBManager:
 
 		Base.prepare(engine, reflect=True)
 
+		self.users = Base.classes.users
 		self.students = Base.classes.students
 		self.classes = Base.classes.classes
 		self.professors = Base.classes.professors
@@ -26,9 +27,8 @@ class DBManager:
 
 		random.seed(datetime.now())
 
-
-	def new_student(self,ssn, name, address, DOB, major):
-		k = None 
+	def new_user(self,ssn,name,address,DOB):
+		k = None
 		for _ in range(100):
 			fname,lname = name.split(' ')
 			uname = fname[0].lower()+lname[:7].lower()
@@ -38,20 +38,19 @@ class DBManager:
 			else:
 				k = 1
 
-
 			email = uname+'@cooper.edu'
 
 			password = 'burp'
+			# TODO: fix this so its not absolute garbage lol
 
-			new_student = self.students(uid = None, ssn=ssn,uname=uname,password=password,name=name,
-										email=email,address=address,date_of_birth=DOB,major=major)
-			# import pdb
-			# pdb.set_trace()
+			new_user = self.users(ssn=ssn, uname=uname, password=password,name=name,email=email,
+									address=address,date_of_birth=DOB)
 
-			self.session.add(new_student)
+			self.session.add(new_user)
+
 			try:
 				self.session.commit()
-				return
+				return new_user.uid,new_user.uname, new_user.email, new_user.password
 			except exc.IntegrityError, e:
 				self.session.rollback()
 				if '\'ssn\'' in e.orig.args[1]:
@@ -60,65 +59,63 @@ class DBManager:
 					continue
 				else:
 					break
-		raise ValueError('Could not complete')
+		raise TimeoutError('Too many attempts')
 
+	def new_student(self, ssn, name, address, DOB, major, grad, uid=None):
+		if uid:
+			new_student = self.students(sid=None, uid=uid, major=major, graduation=grad)
+			self.session.add(new_student)
+			self.session.commit()
+			return
+		uid, uname, email, password = self.new_user(ssn, name, address, DOB)
+		new_student = self.students(sid=None, uid=uid, major=major, graduation=grad)
+		self.session.add(new_student)
+		self.session.commit()
+		return uname, email, password
 
-	def new_professor(self,ssn, name, address, DOB, department, salary):
-		k = None 
-		for _ in range(100):
-			fname,lname = name.split(' ')
-			uname = fname[0].lower()+lname[:7].lower()
-			if k:
-				uname += str(k)
-				k += 1
-			else:
-				k = 1
-
-			s_emails = session.query(self.students.email).select_from(self.students)
-			p_emails = session.query(self.professors.email).select_from(self.professors)
-			a_emails = session.query(self.administrators.email).select_from(self.administrators)
-
-			import pdb
-			pdb.set_trace
-
-			emails = s_emails+p_emails+a_emails
-
-			email = uname+'@cooper.edu'
-
-			password = 'burp'
-
-			new_professor = self.professors(uid = None, ssn=ssn,uname=uname,password=password,name=name,
-										email=email,address=address,date_of_birth=DOB,department=department,
-										salary=salary)
-			# import pdb
-			# pdb.set_trace()
-
+	def new_professor(self,ssn, name, address, DOB, department, salary, uid=None):
+		if uid:
+			new_professor = self.professors(sid=None, uid=uid, department=department, salary=salary)
 			self.session.add(new_professor)
 			self.session.commit()
-				return
-			except exc.IntegrityError, e:
-				self.session.rollback()
-				if '\'ssn\'' in e.orig.args[1]:
-					raise ValueError('Duplicate SSN!')
-				elif '\'email\'' in e.orig.args[1]:
-					continue
-				else:
-					break
-		raise ValueError('Could not complete')
+			return
+		uid, uname, email, password = self.new_user(ssn, name, address, DOB)
+		new_professor = self.professors(sid=None, uid=uid, department=department, salary=salary)
+		self.session.add(new_professor)
+		self.session.commit()
+		return uname, email, password		
 
-	def new_administrator(self,ssn, name, address, DOB):
-		raise NotImplementedError
+	def new_administrator(self,ssn, name, address, DOB, uid=None):
+		if uid:
+			new_administrator = self.administrators(sid=None, uid=uid, department=department)
+			self.session.add(new_professor)
+			self.session.commit()
+			return
+		uid, uname, email, password = self.new_user(ssn, name, address, DOB)
+		new_administrator = self.administrators(sid=None, uid=uid, department=department)
+		self.session.add(new_administrator)
+		self.session.commit()
+		return uname, email, password			
 
-	def new_class(self,cid, semester, meeting_times, department, credits, max_students):
-		raise NotImplementedError
+	def new_class(self, semester, meeting_times, department, credits, max_students):
+		new_class = self.classes(semester=semester,meeting_times=meeting_times,department=department,credits=credits,max_students=max_students)
+		self.session.add(new_class)
+		self.session.commit()
+		return new_class.cid
 
-	def change_password(self,uid, user_type, old_pwd, new_pwd):
-		# TODO: exec query and update old pwd
-		find_query = "SELECT u.password FROM \"{}\" u WHERE u.uid==\"{}\";".format(user_type,uid)
+	def change_password(self, uid, old_pwd, new_pwd):
+		user = self.session.query(self.users).filter(self.users.uid==uid).one()
+		if user.password != old_pwd:
+			raise ValueError('Wrong password')
+		user.password = new_pwd
+		self.session.commit()
 
-	def get_gpa(self,sid):
-		gpa_query = "SELECT SUM(t.grade*c.credits)/SUM(c.credits) FROM (taking t JOIN students s ON t.sid=s.uid AND t.grade IS NOT NULL AND s.uid=\"{}\") \
-					JOIN classes c ON t.cid=c.cid;".format(sid)
+	def get_gpa(self,uid):
+		# gpa_query = "SELECT SUM(t.grade*c.credits)/SUM(c.credits) FROM (taking t JOIN students s ON t.sid=s.uid AND t.grade IS NOT NULL AND s.uid=\"{}\") \
+		# 			JOIN classes c ON t.cid=c.cid;".format(sid)
+		gpa = self.session.query(func.sum(self.taking.grade*self.classes.credits)/func.sum(self.classes.credits)).select_from(self.taking).filter(self.taking.sid==uid,self.taking.grade.isnot(None)).join(self.classes).one()[0]
+		import pdb
+		pdb.set_trace()
 
 	def get_roster(self,cid):
 		roster_query = "SELECT s.name,s.email,s.major FROM taking t JOIN students s on t.sid=s.uid and t.cid=\"{}\";".format(cid)
@@ -126,6 +123,7 @@ class DBManager:
 
 if __name__ == '__main__':
 	dbm = DBManager('root','')
-	dbm.new_student('1139284121','Isaac Alboucai','317 East 9th St, New York, NY 10003',datetime.strptime('10-12-1997','%m-%d-%Y'),'Electrical Engineering')
-	dbm.new_student('1039284121','Isaac Alboucai','317 East 9th St, New York, NY 10003',datetime.strptime('10-12-1997','%m-%d-%Y'),'Electrical Engineering')
-	dbm.new_student('1039284121','Isaac Alboucai','317 East 9th St, New York, NY 10003',datetime.strptime('10-12-1997','%m-%d-%Y'),'Electrical Engineering',3.50)
+	dbm.get_gpa(2)
+	import pdb
+	pdb.set_trace()
+
