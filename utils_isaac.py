c89897289670ab464a2911acfcabebab4273ce2e
gylee1618 <horseantson@hotmail.com>
2018-12-16 10:50:29 -0500

    split utils

---
+++ utils_isaac.py
@@ -0,0 +1,26 @@
+def enrol(sid,cid):
+	raise NotImplementedError
+
+def drop(sid,cid):
+	raise NotImplementedError
+
+def change_salary(uid, new_salary):
+	raise NotImplementedError
+
+def get_schedule(sid,semester):
+	raise NotImplementedError
+
+def get_grades(sid, semeter=None):
+	raise NotImplementedError
+
+def get_class_info(cid)
+:	raise NotImplementedError
+
+def get_prof_info(pid):
+	raise NotImplementedError
+
+def get_student_info(sid):
+	raise NotImplementedError
+
+def get_admin_info(uid):
+	raise NotImplementedError
