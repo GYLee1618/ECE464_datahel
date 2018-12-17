1d72951b4dade3771bd3093e938a77e04afad2c8
gylee1618 <horseantson@hotmail.com>
2018-12-16 22:33:48 -0500

    fix isaacs mistakes

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from utils import DBManager
-from flask import request
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='')
 dbm = DBManager('root','')
 
 @app.route("/")
 def hello():
-	return "hey Isaac"
+	return render_template('index.html')
 @app.route("/path")
 def function_to_run():
-	return "gavin is a butt"
-@app.route("/student_login") methods=['POST']
-def login_student():
+	return 
+
+@app.route("/student_login", methods=['POST'])
+def student_login():
 	uname = request.form['username']
 	pwd = request.form['pass']
 	success = dbm.authenticate(uname,pwd,'students')
@@ -21,16 +21,16 @@ def login_student():
 	else:
 		return "You done fucked up"
 
-@app.route("/professor_login") methods=['POST']
-def login_student(uname,pwd):
+@app.route("/professor_login", methods=['POST'])
+def professor_login(uname,pwd):
 	success = dbm.authenticate(uname,pwd,'professors')
 	if success:
 		return "You In professor"
 	else:
 		return "You done fucked up"
 
-@app.route("/admin_login") methods=['POST']
-def login_student(uname,pwd):
+@app.route("/admin_login", methods=['POST'])
+def admin_login(uname,pwd):
 	success = dbm.authenticate(uname,pwd,'adminstrators')
 	if success:
 		return "You In admin"
