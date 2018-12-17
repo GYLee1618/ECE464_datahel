64d2924e3ddf29c4a853c6559a6a8157b036972d
ialb97 <ialb998@gmail.com>
2018-12-17 13:49:28 -0500

    password hashing

--- utils.py
+++ utils.py
@@ -109,9 +109,9 @@ class DBManager:
 
 	def change_password(self, uid, old_pwd, new_pwd):
 		user = self.session.query(self.users).filter(self.users.uid==uid).one()
-		if user.password != old_pwd:
+		if sha256_crypt.verify(pwd, user.password):
 			raise ValueError('Wrong password')
-		user.password = new_pwd
+		user.password = sha256_crypt.encrypt(new_pwd)
 		self.session.commit()
 
 	def get_gpa(self,uid):
