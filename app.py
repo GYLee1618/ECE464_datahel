3deea51bc9fa5707347e0e8efb49aa8f86a90da0
ialb97 <ialb998@gmail.com>
2018-12-16 23:20:59 -0500

    added session support

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 from utils import DBManager
 
 app = Flask(__name__,template_folder='')
+app.secret_key = "rexrexrex"
 dbm = DBManager('root','')
 
 @app.route("/")
@@ -15,24 +16,33 @@ def function_to_run():
 def student_login():
 	uname = request.form['username']
 	pwd = request.form['pass']
-	success = dbm.authenticate(uname,pwd,'students')
-	if success:
-		return "You In student"
+	uid = dbm.authenticate(uname,pwd,'students')
+	if uid != None:
+		session['uname'] = uname
+		session['uid'] = uid 
+		session['access'] = "student" 
+		return "You In student " + session['uid']
 	else:
 		return "You done fucked up"
 
 @app.route("/professor_login", methods=['POST'])
 def professor_login(uname,pwd):
-	success = dbm.authenticate(uname,pwd,'professors')
-	if success:
+	uid = dbm.authenticate(uname,pwd,'professors')
+	if uid != None:
+		session['uname'] = uname
+		session['uid'] = uid 
+		session['access'] = "professor" 
 		return "You In professor"
 	else:
 		return "You done fucked up"
 
 @app.route("/admin_login", methods=['POST'])
 def admin_login(uname,pwd):
-	success = dbm.authenticate(uname,pwd,'adminstrators')
-	if success:
+	uid = dbm.authenticate(uname,pwd,'adminstrators')
+	if uid != None:
+		session['uname'] = uname
+		session['uid'] = uid 
+		session['access'] = "admin" 
 		return "You In admin"
 	else:
 		return "You done fucked up"
