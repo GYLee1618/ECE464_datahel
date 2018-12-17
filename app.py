91c05775788def61960cb2696375fefdec251ffa
ialb97 <ialb998@gmail.com>
2018-12-16 22:07:04 -0500

    added login functionality

--- app.py
+++ app.py
@@ -2,10 +2,37 @@ from flask import Flask
 from utils import DBManager
 
 app = Flask(__name__)
+dbm = DBManager('root','')
 
 @app.route("/")
 def hello():
 	return "hey Isaac"
+@app.route("/path")
+def function_to_run():
+	return "gavin is a butt"
+@app.route("/student_login")
+def login_student(uname,pwd):
+	success = dbm.authenticate(uname,pwd,'students')
+	if success:
+		return "You In student"
+	else:
+		return "You done fucked up"
+
+@app.route("/professor_login")
+def login_student(uname,pwd):
+	success = dbm.authenticate(uname,pwd,'professors')
+	if success:
+		return "You In professor"
+	else:
+		return "You done fucked up"
+
+@app.route("/admin_login")
+def login_student(uname,pwd):
+	success = dbm.authenticate(uname,pwd,'adminstrators')
+	if success:
+		return "You In admin"
+	else:
+		return "You done fucked up"
 
 if __name__ == '__main__':
 	app.run('localhost',port=9001)
